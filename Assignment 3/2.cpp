658171119cc7e94b2a1b780d9dbcd025502a527c
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-10-09 01:04:16 +0530

    Write a program which accept number from user and print even factors of that number

---
+++ Assignment 3/2.cpp
@@ -0,0 +1,46 @@
+//Write a program which accept number from user and print even factors of that number
+
+//Input : 24        Output : 2 4 6 12
+
+
+#include<iostream>
+using namespace std;
+
+class Code
+{
+    public :
+        int iNo , iCnt;
+
+    Code()
+    {
+        iNo = 0;
+        iCnt = 0;
+    }
+    Code(int iValue)
+    {
+        iNo = iValue;
+    }
+    void DisplayFactor()
+    {
+        for(iCnt = 1 ; iCnt <= iNo ; iCnt++)
+        {
+            if(iNo % iCnt == 0 && iCnt % 2 == 0)
+            {
+                cout<<iCnt<<"\t";
+            }
+        }
+    }
+};
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter number : ";
+    cin>>iValue;
+
+    Code obj(iValue);
+    obj.DisplayFactor();
+
+    return 0;
+}

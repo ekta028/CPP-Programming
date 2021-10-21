938f377beb2b9f71f0e18bd42a9fa855cdc04b4f
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-10-22 00:57:12 +0530

    Accept number from user and display below pattern

---
+++ Assignment 10/4.cpp
@@ -0,0 +1,43 @@
+//Accept number from user and display below pattern
+
+//Input : 5     Output : # 1 * # 2 * # 3 * # 4 * # 5 *
+
+
+#include<iostream>
+using namespace std;
+
+class Code
+{
+    public :
+        int iNo , iCnt;
+
+    Code()
+    {
+        iNo = 0;
+        iCnt = 0;
+    }
+    Code(int iValue)
+    {
+        iNo = iValue;
+    }
+    void Pattern()
+    {
+        for(iCnt = 1 ; iCnt <= iNo ; iCnt++)
+        {
+            cout<<"#"<<"\t"<<iCnt<<"\t"<<"*"<<"\t";
+        }
+    }
+};
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter number of elemnts : ";
+    cin>>iValue;
+
+    Code obj(iValue);
+    obj.Pattern();
+
+    return 0;
+}

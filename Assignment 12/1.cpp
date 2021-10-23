e28eee377ac57a54768bf0ae0b8eff503a9b0b0a
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-10-24 00:38:03 +0530

    Accept number of rows and number of columns from user and display below pattern

---
+++ Assignment 12/1.cpp
@@ -0,0 +1,60 @@
+//Accept number of rows and number of columns from user and display below pattern
+
+//Input : iRow = 4      iCol = 4
+//Output : 
+// A B C D
+// A B C D
+// A B C D
+// A B C D
+
+
+#include<iostream>
+using namespace std;
+
+class Code
+{
+    public :
+        int iRow , iCol , i , j;
+        char cValue;
+
+    Code()
+    {
+        iRow = 0;
+        iCol = 0;
+        i = 0;
+        j = 0;
+        cValue = '\0';
+    }
+    Code(int iValue1 , int iValue2)
+    {
+        iRow = iValue1;
+        iCol = iValue2;
+    }
+    void Pattern()
+    {
+        for(i = 1 ; i <= iRow ; i++)
+        {
+            for(j = 1 , cValue = 'A' ; j <= iCol ; j++ , cValue++)
+            {
+                cout<<cValue<<"\t";
+            }
+            cout<<"\n";
+        }
+    }
+};
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    cout<<"Enter number of rows : ";
+    cin>>iValue1;
+
+    cout<<"Enter number of columns : ";
+    cin>>iValue2;
+
+    Code obj(iValue1,iValue2);
+    obj.Pattern();
+
+    return 0;
+}

92d1c36c29363f9c0ff3938262b10e5d21cf692c
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-10-30 17:21:11 +0530

    Accept N numbers from user and return the smallest number

---
+++ Assignment 20/2.c
@@ -0,0 +1,52 @@
+//Accept N numbers from user and return the smallest number.
+
+//Input : N : 6		Elements : 85 66 3 66 93 88		Output : 3
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Minimum(int Arr[],int iSize)
+{
+	int i = 0 ,	 iMIN = 0;
+	
+	iMIN = Arr[0];
+	
+	for(i = 0 ; i < iSize ; i++)
+	{
+		if(iMIN > Arr[i])
+		{
+			iMIN = Arr[i];
+		}
+	}
+	return iMIN;
+}
+	
+int main()
+{
+	int iSize = 0 , iCnt = 0 ,iValue = 0,iRet = 0; 
+	int *p = NULL;
+	
+	printf("Enter number of elements :");
+	scanf("%d",&iSize);
+	
+	p = (int *)malloc(iSize * sizeof(int));
+	
+	if(p == NULL)
+	{
+		printf("Unable to allocate memory");
+		return -1;
+	}
+	
+	printf("Enter %d elements",iSize);
+	for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+	{
+		scanf("%d",&p[iCnt]);
+	}
+	
+	iRet = Minimum(p,iSize);
+	
+	printf("Minimum number is :%d",iRet);
+	
+	free(p);
+}

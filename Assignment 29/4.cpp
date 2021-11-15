6d090ce636433e95c275c227a32e4bd082069aa9
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-11-16 00:50:01 +0530

    Write a program which accept one number and position from user and toggle that bit.Return modified number

---
+++ Assignment 29/4.cpp
@@ -0,0 +1,58 @@
+//Write a program which accept one number and position from user and toggle bit of that position. Return modified number.
+
+//Input : 10		iPos : 3		Output : 14
+
+
+#include<iostream>
+using namespace std;
+
+class Code
+{
+	public :
+		int iNo , iPos;
+	
+	Code()
+	{
+		iNo = 0;
+		iPos = 0;
+	}
+	Code(int iValue,int Pos)
+	{
+		iNo = iValue;
+		iPos = Pos;
+	}
+	int ToggleBit()
+	{
+		unsigned int iMask = 0x00000001;
+		unsigned int iResult = 0;
+	
+		if((iPos < 1) || (iPos > 32))
+		{
+	
+		}
+		iMask = iMask << (iPos - 1);
+	
+		iResult = iNo ^ iMask;
+	
+		return iResult;
+	}
+};
+
+int main()
+{
+	unsigned int iValue = 0 , iPos = 0;
+	unsigned int iRet = 0;
+	
+	cout<<"Enter number : ";
+	cin>>iValue;
+	
+	cout<<"Enter position : ";
+	cin>>iPos;
+
+	Code obj(iValue,iPos);
+	
+	iRet = obj.ToggleBit();
+	cout<<"Updated number is : "<<iRet<<"\n";
+	
+	return 0;
+}
